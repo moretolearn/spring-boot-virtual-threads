3f505a698f1a87bda9a3e84c03ed66e42e4942ee
Y RAM MOHAN REDDY <yanumularammohan661@gmail.com>
2024-06-24 10:56:59 +0530

    Third Commit

--- src/main/java/com/moretolearn/SpringBootVirtualThreadsApplication.java
+++ src/main/java/com/moretolearn/SpringBootVirtualThreadsApplication.java
@@ -2,10 +2,8 @@ package com.moretolearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class SpringBootVirtualThreadsApplication {
 
 	public static void main(String[] args) {
